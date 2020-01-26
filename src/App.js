ea6fc7fce2544022d6c0039fff0eedbc8c9a905a
pdr-clv <pdr.clv@gmail.com>
2020-01-26 22:01:33 +0300

    created project from create react and added files from homepage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Homepage from './homepage.component';
+
+import './App.css';
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
