83150f13bdffc47964b8dcbcb9144064353a16d7
pdr-clv <pdr.clv@gmail.com>
2020-02-04 11:20:15 +0300

    created cart-item component and connected our cart dropdown with cartItems reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -15,6 +15,7 @@ export const addItemToCart = (cartItems,cartItemToAdd) => {
     )
   } else {
 // este return, lo que hace es hacer un push a todos los cartItems con lo que está detrás de la coma. Si se pasara un cartitems null, o que no estuviera, se añadería la propiedad quantity = 1. De esta manera siempre vamos a tener quantity en todas los cartItems que haya en el store.
+// importante, para que funcione el push, poner [...cartitems], no olvidar los 3 puntitos ...
     return [...cartItems, { ...cartItemToAdd , quantity:1 }]
   }
 }
