bc6a92b74baa074a0c52cc8b12c48c2a0d33369e
pdr-clv <pdr.clv@gmail.com>
2020-02-18 23:40:20 +0300

    updating stripe integration and updaging our favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import './checkout.styles.scss';
@@ -33,6 +34,12 @@ const CheckoutPage = ({cartItems , total}) => (
     <div className='total'>
       Total: ${total}
     </div>
+    <div class='text-warning'>
+    *Please use the following test credit card for payments *<br/>
+    4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
